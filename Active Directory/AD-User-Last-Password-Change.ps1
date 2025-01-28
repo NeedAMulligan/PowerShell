bce475225d6b48b9a503a59e1bb9160085fae784
NeedAMulligan <68872562+NeedAMulligan@users.noreply.github.com>
2025-01-28 11:52:03 -0500

    Update AD-User-Last-Password-Change.ps1

--- Active Directory/AD-User-Last-Password-Change.ps1
+++ Active Directory/AD-User-Last-Password-Change.ps1
@@ -1,3 +1,3 @@
-﻿Get-ADUser -filter * -searchbase "OU=Executive,OU=Superior Paving Employees,DC=superiorpaving,DC=net" -properties passwordlastset, passwordneverexpires, CanonicalName | sort-object name, CanonicalName | select-object Name, CanonicalName, passwordlastset, passwordneverexpires | Export-csv -path c:\temp\Executive-user-password-info-20190531.csv
+﻿Get-ADUser -filter * -searchbase "OU=UPDATE-INFO,OU=UPDATE-INFO,DC=UPDATE-INFO,DC=net" -properties passwordlastset, passwordneverexpires, CanonicalName | sort-object name, CanonicalName | select-object Name, CanonicalName, passwordlastset, passwordneverexpires | Export-csv -path c:\temp\UPDATE-INFO.csv
 Start-Sleep -s 5
 Get-ChildItem -Filter C:\temp\*.csv | Select-Object -ExpandProperty FullName | Import-Csv | Export-Csv c:\temp\merged.csv -NoTypeInformation -Append

d08adbfa8e44da7174a958b39490029d3efbd1c8
NeedAMulligan <68872562+NeedAMulligan@users.noreply.github.com>
2025-01-28 13:16:37 -0500

    Add files via upload

---
+++ Group Policy/GPOBackup.ps1
@@ -0,0 +1,9 @@
+﻿# Backup all GPO
+$date = Get-Date -Format yyyyMMdd
+New-Item C:\GPO_Backup -ItemType Directory
+New-Item C:\GPO_Backup\$date -ItemType Directory
+$GPOS = Get-GPO -All | Select DisplayName
+foreach ($GPO in $GPOS) {
+    New-Item C:\GPO_Backup\$date\$GPO -ItemType Directory
+    Backup-GPO -Name $GPO.Displayname -Path C:\GPO_Backup\$date\$GPO
+}

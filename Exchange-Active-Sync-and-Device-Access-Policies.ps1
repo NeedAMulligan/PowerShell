11af675a9901e785ed0beed7922fbe5869b9ee43
NeedAMulligan <68872562+NeedAMulligan@users.noreply.github.com>
2025-05-14 08:52:18 -0400

    Add files via upload

---
+++ Exchange-Active-Sync-and-Device-Access-Policies.ps1
@@ -0,0 +1,5 @@
+New-ActiveSyncDeviceAccessRule -Characteristic DeviceModel -QueryString "Outlook for iOS and Android" -AccessLevel Quarantine
+New-ActiveSyncDeviceAccessRule -Characteristic DeviceType -QueryString "Android" -AccessLevel Quarantine
+New-ActiveSyncDeviceAccessRule -Characteristic DeviceType -QueryString "iPad" -AccessLevel Quarantine
+New-ActiveSyncDeviceAccessRule -Characteristic DeviceType -QueryString "iPhone" -AccessLevel Quarantine
+New-ActiveSyncDeviceAccessRule -Characteristic DeviceType -QueryString "iPod" -AccessLevel Quarantine

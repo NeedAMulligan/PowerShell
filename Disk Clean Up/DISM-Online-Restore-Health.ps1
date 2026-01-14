a570b1ad95ff989f27ac51f1fe674f0cf8bd708c
NeedAMulligan <68872562+NeedAMulligan@users.noreply.github.com>
2026-01-14 16:11:19 -0500

    Update DISM-Online-Restore-Health.ps1

--- Disk Clean Up/DISM-Online-Restore-Health.ps1
+++ Disk Clean Up/DISM-Online-Restore-Health.ps1
@@ -2,4 +2,8 @@ Repair-WindowsImage -Online -RestoreHealth
 
 # see logs in realtime for DISM process #
 
-Get-Content -Path C:\Windows\Logs\DISM\dism.log -Wait
+Repair-WindowsImage -Online -RestoreHealth
+
+# see logs in realtime for DISM process #
+
+Get-Content -Path C:\Windows\Logs\CBS\CBS.log -tail 10 -wait

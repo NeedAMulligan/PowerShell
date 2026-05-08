06efa4147afb40455eebd5eb9d9535c554f61d2a
NeedAMulligan <68872562+NeedAMulligan@users.noreply.github.com>
2026-05-08 14:20:01 -0400

    Delete MS Teams/MS-Teams-Get-Groups-User-Owner.ps1

--- MS Teams/MS-Teams-Get-Groups-User-Owner.ps1
+++
@@ -1,9 +0,0 @@
-$teams = Get-Team
-$count = 0
-foreach ($team in $teams) {  
-    $owner = Get-TeamUser -GroupId $team.GroupId -Role Owner
-    if ($owner.User -eq "Tamara.Williams") {
-        $team.DisplayName
-        $count++
-    }
-} "found $count teams where the target user is owner"

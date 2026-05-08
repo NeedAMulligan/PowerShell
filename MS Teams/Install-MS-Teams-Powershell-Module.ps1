3a30d503ff712f018103e4d64627c86b9ed97458
NeedAMulligan <68872562+NeedAMulligan@users.noreply.github.com>
2026-05-08 14:19:25 -0400

    Delete MS Teams/Install-MS-Teams-Powershell-Module.ps1

--- MS Teams/Install-MS-Teams-Powershell-Module.ps1
+++
@@ -1,8 +0,0 @@
-#Run the following command to install the latest PowerShellGet
-
-Install-Module -Name PowerShellGet -Force -AllowClobber
-
-#Install the Teams PowerShell Module
-
-Install-Module -Name MicrosoftTeams -Force -AllowClobber
-

abf25b52bec7311243145719f7d23cf97f486163
NeedAMulligan <68872562+NeedAMulligan@users.noreply.github.com>
2026-05-09 11:24:03 -0400

    Update Get-LockedOutUser.ps1

--- Get Lock Out User/Get-LockedOutUser.ps1
+++ Get Lock Out User/Get-LockedOutUser.ps1
@@ -5,6 +5,16 @@
 .DESCRIPTION
     Get-LockedOutUser.ps1 is an advanced script that returns a list of users who were locked out in Active Directory
 by querying the event logs on the PDC emulation in the domain.
+
+This “Get-LockedOutUser.ps1“ script allows you to specify the following via parameter input to narrow down the results:
+
+Specific userid, defaulting to all locked out userid’s
+Start time to begin searching records for, defaulting to the last three days
+Domain name to search for lockouts in, defaulting to the user’s domain who is running the script
+
+Note: This script uses the "Using" scope modifier which is a feature that was introduced in PowerShell version 3 so that is the minimum requirement. Note that the error: "A null value was encountered in the StartTime hash table key. Null values are not permitted." will be generated if you attempt to run this script from a machine that is running PowerShell version 2.
+
+For more information about this script, visit my blog article about it: "PowerShell Script to Determine What Device is Locking Out an Active Directory User Account".
  
 .PARAMETER UserName
     The userid of the specific user you are looking for lockouts for. The default is all locked out users.

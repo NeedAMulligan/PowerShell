d11f8a717e2a23d064b4f32c64647a3499d2a057
NeedAMulligan <68872562+NeedAMulligan@users.noreply.github.com>
2026-05-09 16:00:45 -0400

    Add files via upload

---
+++ Windows/WindowsUpdateRebootMessage.ps1
@@ -0,0 +1,10 @@
+﻿Add-Type -AssemblyName PresentationCore,PresentationFramework
+$ButtonType = [System.Windows.MessageBoxButton]::OK
+$MessageboxTitle = “From Rees Broome IT Dept”
+$Messageboxbody = “Microsoft Windows Updates have been applied and your computer needs to be restarted to finish installing them. Please restart your computer ASAP. 
+
+This message will repeat every 6 hours until your system has been restarted.
+
+Thanks”
+$MessageIcon = [System.Windows.MessageBoxImage]::Warning
+[System.Windows.MessageBox]::Show($Messageboxbody,$MessageboxTitle,$ButtonType,$messageicon)
